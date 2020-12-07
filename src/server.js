a3cf9c7fd286affa2583d18a3efa0783cd533fc7
leandrofahur <leandrofahur@gmail.com>
2020-12-06 17:17:46 -0800

    CRUD profile implemented

--- src/server.js
+++ src/server.js
@@ -1,11 +1,14 @@
-const connectDB = require("./config/db");
+const connectDB = require("../config/db");
 const express = require("express");
 const app = express();
-
+app.use(express.json({ extended: false }));
 const PORT = process.env.PORT || 3000;
 
 // connect to the db:
 connectDB();
+
+// Configure routes:
+app.use("/api/profile", require("./routes/api/profile"));
 
 app.get("/", (req, res) => {
   res.status(200).send("API request");
